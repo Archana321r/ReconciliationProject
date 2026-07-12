4f8df1af295a193a6c1af672c02df49da58990f2
Archana321r <archanarathod729@gmail.com>
2026-07-12 20:40:10 +0530

    Add files via upload

---
+++ class AllBlock.java
@@ -0,0 +1,32 @@
+class Product 
+{
+
+public void displayproductdetails(){
+system.out.printIn("15 scooty 99000");
+}
+
+public void displayorderdetails(){
+system.out.printIn("id:scooty#321 Date:6/01/2025 Location:Pune");
+}
+
+public void displayCustomerdetails(){
+system.out.printIn("Name:Archana Contact:454654658 email:archanarathod729@gmail.com");
+}
+
+public static void main (string)[] args)
+{
+Product p=new Product();
+p.displayproductdetails();
+p.displayproductdetails();
+p.displayproductdetails();
+
+}
+
+
+}
+
+
+
+}
+
+ 
---
+++ class variables.java
@@ -0,0 +1,14 @@
+class variables {
+
+int rollno= 10; // global variable
+string name = archana 
+
+
+public static void main (string[] args)
+{
+system.out.printIn("---variabletestprogram----");
+
+}
+
+}
+
